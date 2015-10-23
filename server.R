e65ab58ce746b19e15deca9bce2f9aef18563bff
Jessica Peterka-Bonetta <jessicabonetta@hotmail.com>
2015-10-23 15:25:29 +0200

    delete function sourcing and oauth

--- server.R
+++ server.R
@@ -6,30 +6,10 @@ library(RJSONIO)
 library(dplyr)
 library(magrittr)
 library(stringr)
-source("hashgrep.R")
-source("getUserMedia.R")
 source("getScore.R")
 
 # cache
 options(httr_oauth_cache = TRUE)
-
-# load instagram oauth
-load("ig_oauth_ia")
-token <- ig_oauth_ia$token
-
-# load twitter oauth 
-load("twitterOauthSurvey.Rdata")
-consumer_key <- my_oauth$consumerKey
-consumer_secret <- my_oauth$consumerSecret
-access_token <- my_oauth$oauthKey
-access_secret <- my_oauth$oauthSecret
-setup_twitter_oauth(consumer_key = consumer_key, consumer_secret = consumer_secret,
-                    access_token = access_token, access_secret = access_secret)
-1
-
-# read in lists of words
-pos <- readRDS("data/poswords.rds")
-neg <- readRDS("data/negwords.rds")
 
 shinyServer(
     function(input, output) {
@@ -40,6 +20,7 @@ shinyServer(
             igname <- isolate(input$instagram)
             
             score <- getScore(twittername = twittername, igname = igname)
+            
             
         })
         
